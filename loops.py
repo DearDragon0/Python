0cc494044af516aade5e515731c0685256c34f18
DearDragon0 <113325394+DearDragon0@users.noreply.github.com>
2025-08-04 21:47:45 +0530

    Update loops.py
    while loop in python

--- loops.py
+++ loops.py
@@ -11,3 +11,9 @@ while j < 5:
     if j == 3:
         continue
     print(j)
+
+# Example of a while loop
+count = 0
+while count < 5:
+    print(count)
+    count += 1  # Increment the count to eventually make the condition false
