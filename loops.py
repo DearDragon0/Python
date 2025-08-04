b0432979fefe11fd543f926a1d9b1bf8aa2d8169
DearDragon0 <113325394+DearDragon0@users.noreply.github.com>
2025-08-04 21:46:45 +0530

    Create loops.py
    Loops in python

---
+++ loops.py
@@ -0,0 +1,13 @@
+# Example of break in a for loop
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+# Example of continue in a while loop
+j = 0
+while j < 5:
+    j += 1
+    if j == 3:
+        continue
+    print(j)
